afdb3b71adb860d1a0117a8cfb0b1db2a89f10da
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-08-28 22:42:04 +0530

    Update Background.js

--- Background.js
+++ Background.js
@@ -1,15 +1,1 @@
-document.getElementById('runQuery').addEventListener('click', () => {
-    const query = document.getElementById('query').value;
-    console.log('Sending query:', query);
 
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const activeTab = tabs[0];
-        chrome.scripting.executeScript({
-            target: { tabId: activeTab.id },
-            func: (query) => {
-                window.postMessage({ type: 'RUN_QUERY', query: query }, '*');
-            },
-            args: [query]
-        });
-    });
-});
